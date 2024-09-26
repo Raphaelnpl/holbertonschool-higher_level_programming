788c3520f6b2393208f476bb5520358e0a1816f1
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-09-26 08:56:03 +0000

    change

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-'''
-Module containing the function inherits_from
-'''
+
+"""this function returns True if \
+the object is a class \
+that inherited (directly or indirectly)"""
+
 
 def inherits_from(obj, a_class):
-    '''
-    Returns True if obj is an instance of a class that inherited
-    (directly or indirectly) from the specified class, False otherwise.
-    '''
+    """this method returns True if \class \
+that inherited (directly or indirectly)"""
+
     return isinstance(obj, a_class) and type(obj) is not a_class
-touch 
