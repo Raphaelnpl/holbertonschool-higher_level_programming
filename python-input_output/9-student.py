946749bc707f749b3ed44ba253f7d6c49d16a9c0
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-10-03 09:14:28 +0000

    change

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,12 +1,12 @@
+#!/usr/bin/python3
 class Student:
     """A class that defines a student by first name, last name, and age"""
-
+    
     def __init__(self, first_name, last_name, age):
-        """Initializes a Student instance with first_name, last_name, and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
+    
     def to_json(self):
         """Retrieves a dictionary representation of a Student instance"""
         return self.__dict__
